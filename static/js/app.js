ac996bae91095d11d930b2f3c31c58155afabbd6
Your Name <sabrina.avila.09@gmail.com>
2020-09-28 13:34:07 -0700

    update

--- static/js/app.js
+++ static/js/app.js
@@ -1,10 +1,5 @@
-// from data.js
-var tableData = data;
-
 var tbody = d3.select("tbody");
 
-// console.log(data);
-// YOUR CODE HERE!
 data.forEach((ufoReport) => {
     var row = tbody.append("tr");
     Object.entries(ufoReport).forEach(([key, value]) => {
@@ -14,21 +9,43 @@ data.forEach((ufoReport) => {
   });
 
 var button = d3.select("#filter-btn");
-function handleClick() {
-    console.log("A button was clicked!");
-console.log(d3.event.target);
+button.on("click", runEnter);
+
+function runEnter() {
+  d3.event.preventDefault();
+  var inputElement = d3.select("#datetime");
+  var inputValue = inputElement.property("value");
+  console.log(inputValue);
+  tbody.html("");
+
+  var filteredData = data.filter(dates => dates.datetime === inputValue)
+  filteredData.forEach((ufoReport) => {
+    var row = tbody.append("tr");
+    Object.entries(ufoReport).forEach(([key, value]) => {
+      var cell = row.append("td");
+      cell.text(value);
+    });
+  });
+  console.log(filteredData);
 }
-button.on("click", handleClick);
 
-d3.event.preventDefault();
+var button = d3.select("#filter-btn");
+button.on("click", runCity);
 
-var inputElement = d3.select("#datetime");
+function runCity() {
+  d3.event.preventDefault();
+  var inputElement = d3.select("#city");
+  var inputValue = inputElement.property("value");
+  console.log(inputValue);
+  tbody.html("");
 
-var inputValue = inputElement.property("value");
-
-console.log(inputValue);
-console.log(tableData);
-
-var filteredData = tableData.filter(tableData => tableData.datetime === inputValue)
-
-console.log(filteredData);
+  var filteredData = data.filter(cities => cities.city === inputValue)
+  filteredData.forEach((ufoReport) => {
+    var row = tbody.append("tr");
+    Object.entries(ufoReport).forEach(([key, value]) => {
+      var cell = row.append("td");
+      cell.text(value);
+    });
+  });
+  console.log(filteredData);
+}
