57007031b66d6850cdded213efc586d8ed3e0064
Your Name <sabrina.avila.09@gmail.com>
2020-09-28 13:36:49 -0700

    new upadte

--- static/js/app.js
+++ static/js/app.js
@@ -49,3 +49,66 @@ function runCity() {
   });
   console.log(filteredData);
 }
+
+var button = d3.select("#filter-btn");
+button.on("click", runState);
+
+function runState() {
+  d3.event.preventDefault();
+  var inputElement = d3.select("#state");
+  var inputValue = inputElement.property("value");
+  console.log(inputValue);
+  tbody.html("");
+
+  var filteredData = data.filter(states => states.state === inputValue)
+  filteredData.forEach((ufoReport) => {
+    var row = tbody.append("tr");
+    Object.entries(ufoReport).forEach(([key, value]) => {
+      var cell = row.append("td");
+      cell.text(value);
+    });
+  });
+  console.log(filteredData);
+}
+
+var button = d3.select("#filter-btn");
+button.on("click", runCountry);
+
+function runCountry() {
+  d3.event.preventDefault();
+  var inputElement = d3.select("#country");
+  var inputValue = inputElement.property("value");
+  console.log(inputValue);
+  tbody.html("");
+
+  var filteredData = data.filter(countries => countries.country === inputValue)
+  filteredData.forEach((ufoReport) => {
+    var row = tbody.append("tr");
+    Object.entries(ufoReport).forEach(([key, value]) => {
+      var cell = row.append("td");
+      cell.text(value);
+    });
+  });
+  console.log(filteredData);
+}
+
+var button = d3.select("#filter-btn");
+button.on("click", runCity);
+
+function runCity() {
+  d3.event.preventDefault();
+  var inputElement = d3.select("#city");
+  var inputValue = inputElement.property("value");
+  console.log(inputValue);
+  tbody.html("");
+
+  var filteredData = data.filter(cities => cities.city === inputValue)
+  filteredData.forEach((ufoReport) => {
+    var row = tbody.append("tr");
+    Object.entries(ufoReport).forEach(([key, value]) => {
+      var cell = row.append("td");
+      cell.text(value);
+    });
+  });
+  console.log(filteredData);
+}
